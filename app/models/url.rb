b484e518c61b418070cd1f07f11f94cb3107ab02
limchiahooi <limchiahooi@gmail.com>
2017-12-28 15:38:53 +0800

    set up validation and method in Url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,23 @@
+require 'securerandom'
+
 class Url < ApplicationRecord
+
+before_create :shorten
+validates :long_url, :presence => true, :format => URI::regexp(%w(http https))
+validates :long_url, :uniqueness => true
+validates :short_url, :uniqueness => true
+
+  def shorten
+  	self.short_url = SecureRandom.hex(3)
+  # 	write a method here
+  #		self.short_url = "asd"
+  end
+
+
+def count
+	self.click_count += 1
+	self.save
+end
+
+
 end
