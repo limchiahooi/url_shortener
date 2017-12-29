50073fae7f946a1b6ef0f9c6ee65a82e052817d8
limchiahooi <limchiahooi@gmail.com>
2017-12-29 11:50:35 +0800

    redirect short url
    1. Define a new route called "short"
    2. Create short method in urls.controller.rb
    3. Add short action in short url in index page

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
 
 root 'urls#index'
 
-
-
 resources :urls
+
+get 'urls/:id/short' => 'urls#short'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
